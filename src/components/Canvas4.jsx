98c9f1eb1825d92e267d31916db7c38d8c58a7a1
J_ Keyy <ashnazg1212@gmail.com>
2023-12-10 14:13:20 +0000

    feat: Update Canvas4 Component
    Updated loop, color, and background in Canvas4 component. Reduced particle radius.

--- src/components/Canvas4.jsx
+++ src/components/Canvas4.jsx
@@ -9,7 +9,7 @@ export default function Canvas4(params) {
   const handleClick = (e) => {
     mouse.current.x = e.clientX;
     mouse.current.y = e.clientY;
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 10; i++) {
       particles.push(generateRandomParticle({ canvas, mouse }));
     }
   };
@@ -41,19 +41,22 @@ function generateRandomParticle({ canvas, mouse }) {
   // const y = Math.random() * canvas.current.height + radius;
   const x = mouse.current.x + Math.random() * 20;
   const y = mouse.current.y + Math.random() * 20;
-  const color = `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${
-    Math.random() * 255
-  }, 1)`;
+  // const color = `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${
+  //   Math.random() * 255
+  // }, 1)`;
+  const color = "white";
 
   return { x, y, speedX, speedY, radius, color };
 }
 
 function draw(ctx, frameCount, ratio) {
   // console.log(particles.length);
-  ctx.clearRect(0, 0, ctx.canvas.width * ratio, ctx.canvas.height * ratio);
+  // ctx.clearRect(0, 0, ctx.canvas.width * ratio, ctx.canvas.height * ratio);
+  ctx.fillStyle = "rgba(0,0,0,0.1)";
+  ctx.fillRect(0, 0, ctx.canvas.width * ratio, ctx.canvas.height * ratio);
   particles.forEach((particle) => {
     particle.radius > 0.3 ? drawParticle({ ctx, particle }) : null;
-    particle.radius -= 0.1;
+    particle.radius -= 0.08;
     particle.x += particle.speedX;
     particle.y += particle.speedY;
   });
