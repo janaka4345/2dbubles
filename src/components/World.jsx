749fce1bcd6e88280768d36c94c1a4a45a1b098b
J_ Keyy <ashnazg1212@gmail.com>
2023-12-10 11:51:08 +0000

    fix:dissapearing bubbles from canvas 3

--- src/components/World.jsx
+++ src/components/World.jsx
@@ -1,5 +1,6 @@
 import Canvas2 from "./Canvas2";
 import Canvas3 from "./Canvas3";
+import Canvas4 from "./Canvas4";
 
 // import Canvas1 from "./Canvas1";
 export default function World(props) {
@@ -8,7 +9,8 @@ export default function World(props) {
     <>
       {/* <Canvas1 /> */}
       {/* <Canvas2 /> */}
-      <Canvas3 />
+      {/* <Canvas3 /> */}
+      <Canvas4 />
     </>
   );
 }
