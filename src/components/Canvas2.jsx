320cca1967ea95c39364b5f587dffaf7044e99f7
J_ Keyy <ashnazg1212@gmail.com>
2023-12-10 01:56:35 +0000

    feat: Add mouse event handling
    Import useRef hook, handle mouse click events, update canvas arc position

--- src/components/Canvas2.jsx
+++ src/components/Canvas2.jsx
@@ -1,5 +1,12 @@
+import { useRef } from "react";
 import useCanvas from "./useCanvas";
 export default function Canvas2(params) {
+  const mouse = useRef({ x: undefined, y: undefined });
+  const handleClick = (e) => {
+    mouse.current.x = e.clientX;
+    mouse.current.y = e.clientY;
+  };
+
   const draw = (ctx, frameCount, ratio) => {
     ctx.clearRect(0, 0, ctx.canvas.width * ratio, ctx.canvas.height * ratio);
     ctx.fillStyle = "#ff0000";
@@ -8,14 +15,19 @@ export default function Canvas2(params) {
     // ctx.fillRect(10, 10, 100, 100);
 
     ctx.beginPath();
-    ctx.arc(100, 100, 50, 0, Math.PI * 2);
+    ctx.arc(mouse.current.x, mouse.current.y, 50, 0, Math.PI * 2);
     ctx.fill();
     ctx.stroke();
   };
   const canvas = useCanvas(draw);
   return (
     <>
-      <canvas id="canvas1" ref={canvas} />
+      <canvas
+        id="canvas1"
+        ref={canvas}
+        // onClick={handleClick}
+        onMouseMove={handleClick}
+      />
     </>
   );
 }
