679fb366607d42cd0c00a8fb29e1fe4874ee9737
J_ Keyy <ashnazg1212@gmail.com>
2023-12-10 02:30:58 +0000

    feat: Update Canvas2  to paint
    Add mouse down/up events and adjust circle size

--- src/components/Canvas2.jsx
+++ src/components/Canvas2.jsx
@@ -1,23 +1,27 @@
 import { useRef } from "react";
 import useCanvas from "./useCanvas";
 export default function Canvas2(params) {
+  const isMouseDown = useRef(false);
   const mouse = useRef({ x: undefined, y: undefined });
   const handleClick = (e) => {
-    mouse.current.x = e.clientX;
-    mouse.current.y = e.clientY;
+    if (isMouseDown.current) {
+      mouse.current.x = e.clientX;
+      mouse.current.y = e.clientY;
+    }
+    // isMouseDown.current ? console.log(e.clientX) : null;
   };
 
   const draw = (ctx, frameCount, ratio) => {
-    ctx.clearRect(0, 0, ctx.canvas.width * ratio, ctx.canvas.height * ratio);
+    // ctx.clearRect(0, 0, ctx.canvas.width * ratio, ctx.canvas.height * ratio);
     ctx.fillStyle = "#ff0000";
-    ctx.strokeStyle = "white";
-    ctx.lineWidth = 5;
+    // ctx.strokeStyle = "white";
+    // ctx.lineWidth = 5;
     // ctx.fillRect(10, 10, 100, 100);
 
     ctx.beginPath();
-    ctx.arc(mouse.current.x, mouse.current.y, 50, 0, Math.PI * 2);
+    ctx.arc(mouse.current.x, mouse.current.y, 25, 0, Math.PI * 2);
     ctx.fill();
-    ctx.stroke();
+    // ctx.stroke();
   };
   const canvas = useCanvas(draw);
   return (
@@ -26,6 +30,8 @@ export default function Canvas2(params) {
         id="canvas1"
         ref={canvas}
         // onClick={handleClick}
+        onMouseUp={() => (isMouseDown.current = false)}
+        onMouseDown={() => (isMouseDown.current = true)}
         onMouseMove={handleClick}
       />
     </>
