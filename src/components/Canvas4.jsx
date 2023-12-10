04e4098d034ab2059a9d288513a5b7bd0cccf722
J_ Keyy <ashnazg1212@gmail.com>
2023-12-10 12:43:47 +0000

    refactor: Refactor Canvas3
    Refactor Canvas3  component to remove commented out code and organize functions

--- src/components/Canvas4.jsx
+++ src/components/Canvas4.jsx
@@ -1,59 +1,22 @@
 import { useEffect, useRef } from "react";
 import useCanvas from "./useCanvas";
+const particles = [];
 export default function Canvas4(params) {
   // console.log("ren");
   const canvas = useCanvas(draw);
   // const mouse = useRef({ x: undefined, y: undefined });
-
-  const particles = [];
-
-  const generateRandomParticle = () => {
-    const speedX = Math.random() - 0.5;
-    const speedY = Math.random() - 0.5;
-    const radius = Math.random() * 5 + 1;
-    const x = Math.random() * canvas.current.width + radius;
-    const y = Math.random() * canvas.current.height + radius;
-    const color = `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${
-      Math.random() * 255
-    }, 1)`;
-
-    return { x, y, speedX, speedY, radius, color };
-  };
 
   // const handleClick = (e) => {
   //   mouse.current.x = e.clientX;
   //   mouse.current.y = e.clientY;
   // };
 
-  const drawParticle = ({ ctx, particle }) => {
-    ctx.beginPath();
-    ctx.arc(particle.x, particle.y, particle.radius, 0, Math.PI * 2);
-    ctx.fillStyle = particle.color;
-    ctx.fill();
-  };
   useEffect(() => {
     for (let i = 0; i < 100; i++) {
-      particles.push(generateRandomParticle());
+      particles.push(generateRandomParticle(canvas));
     }
   }, []);
 
-  function draw(ctx, frameCount, ratio) {
-    // console.log(particles);
-    ctx.clearRect(0, 0, ctx.canvas.width * ratio, ctx.canvas.height * ratio);
-    particles.forEach((particle) => {
-      drawParticle({ ctx, particle });
-      particle.x > ctx.canvas.width || particle.x < particle.radius
-        ? (particle.speedX *= -1)
-        : null;
-
-      particle.y > ctx.canvas.height || particle.y < particle.radius
-        ? (particle.speedY *= -1)
-        : null;
-
-      particle.x += particle.speedX;
-      particle.y += particle.speedY;
-    });
-  }
   return (
     <>
       <canvas
@@ -63,4 +26,41 @@ export default function Canvas4(params) {
       />
     </>
   );
+}
+
+function generateRandomParticle(canvas) {
+  const speedX = Math.random() - 0.5;
+  const speedY = Math.random() - 0.5;
+  const radius = Math.random() * 5 + 1;
+  const x = Math.random() * canvas.current.width + radius;
+  const y = Math.random() * canvas.current.height + radius;
+  const color = `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${
+    Math.random() * 255
+  }, 1)`;
+
+  return { x, y, speedX, speedY, radius, color };
+}
+
+function draw(ctx, frameCount, ratio) {
+  // console.log(particles);
+  ctx.clearRect(0, 0, ctx.canvas.width * ratio, ctx.canvas.height * ratio);
+  particles.forEach((particle) => {
+    drawParticle({ ctx, particle });
+    particle.x > ctx.canvas.width || particle.x < particle.radius
+      ? (particle.speedX *= -1)
+      : null;
+
+    particle.y > ctx.canvas.height || particle.y < particle.radius
+      ? (particle.speedY *= -1)
+      : null;
+
+    particle.x += particle.speedX;
+    particle.y += particle.speedY;
+  });
+}
+function drawParticle({ ctx, particle }) {
+  ctx.beginPath();
+  ctx.arc(particle.x, particle.y, particle.radius, 0, Math.PI * 2);
+  ctx.fillStyle = particle.color;
+  ctx.fill();
 }
